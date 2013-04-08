501cb80cee2ae28173ce35412a4c5cbdf41fcc4a
Jay States <jstates@based.ca>
2013-04-08 17:40:34 -0400

    use input instead of raw_input (more python3)

--- haystack/management/commands/clear_index.py
+++ haystack/management/commands/clear_index.py
@@ -3,6 +3,10 @@ import sys
 
 from django.core.management.base import BaseCommand
 
+# Python 2 backwards-compatibility
+if sys.version_info < (3, 0, 0):
+    input = raw_input
+
 
 class Command(BaseCommand):
     help = "Clears out the search index completely."
@@ -32,7 +36,7 @@ class Command(BaseCommand):
             print "WARNING: This will irreparably remove EVERYTHING from your search index in connection '%s'." % "', '".join(using)
             print "Your choices after this are to restore from backups or rebuild via the `rebuild_index` command."
 
-            yes_or_no = raw_input("Are you sure you wish to continue? [y/N] ")
+            yes_or_no = input("Are you sure you wish to continue? [y/N] ")
             print
 
             if not yes_or_no.lower().startswith('y'):
